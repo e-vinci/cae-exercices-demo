5107cb77caa5386a4afd63a28b6b2cbd296c86c4
Sébastien Strebelle <sebastien.strebelle@vinci.be>
2024-10-16 20:13:27 +0200

    Empty project from spring initializr

---
+++ complete/src/main/java/be/vinci/ipl/cae/demos/complete/CompleteApplication.java
@@ -0,0 +1,13 @@
+package be.vinci.ipl.cae.demos.complete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompleteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompleteApplication.class, args);
+    }
+
+}
