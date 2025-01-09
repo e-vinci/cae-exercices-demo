891b1f5bb8ac3cea448e016137a5c6710d25e591
Sébastien Strebelle <sebastien.strebelle@vinci.be>
2025-01-09 16:41:47 +0100

    Added Spring Security

--- complete/src/main/java/be/vinci/ipl/cae/demos/complete/repositories/IngredientRepository.java
+++
@@ -1,9 +0,0 @@
-package be.vinci.ipl.cae.demos.complete.repositories;
-
-import be.vinci.ipl.cae.demos.complete.models.entities.Ingredient;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IngredientRepository extends CrudRepository<Ingredient, String> {
-}
